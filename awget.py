e86a945148b7e1e5748f55adad50fe7a70aa14c6
elavertu9 <elavertu@rams.colostate.edu>
2019-11-18 16:50:21 -0700

    initiailzed with base files

---
+++ awget.py
@@ -0,0 +1,16 @@
+import sys
+# Takes up to 2 command line args
+    # awget <URL> [-c chainfile]
+    # if no chainfile supplied, read the local file called chaingang.txt
+    # else if problems with file, print error and exit
+
+argv = sys.argv[1:]
+argn = 0
+
+def main():
+    print("Hello from awget.py")
+
+
+
+if __name__ == '__main__':
+    main()
