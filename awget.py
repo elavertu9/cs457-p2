537d5b94e3506928ab4c2e4536bcc2eef3a26cf7
elavertu9 <elavertu@rams.colostate.edu>
2019-11-18 17:50:08 -0700

    passed random ss information to createconnection to establish a port

--- awget.py
+++ awget.py
@@ -1,5 +1,5 @@
-import sys
-import getopt
+import sys, random, socket
+
 # Takes up to 2 command line args
     # awget <URL> [-c chainfile]
     # if no chainfile supplied, read the local file called chaingang.txt
@@ -7,6 +7,33 @@ import getopt
 
 def printUsage():
     print('Usage: awget.py <URL> -c <chainfile>')
+
+
+def createConnection(address, port):
+    print(address)
+    print(port)
+    # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    #     sock.bind((address, port))
+
+
+
+def readFile(URL, fileName = "chaingang.txt"):
+    hopList = []
+
+    try:
+        with open(fileName, "r") as file:
+            for line in file:
+                hopList.append(line.rstrip("\n\r"))
+        numHops = int(hopList.pop(0))
+        randomSS = random.randint(0, numHops - 1)
+        nextHop = hopList[randomSS]
+        ssInfo = nextHop.split(" ")
+        ssAddress = ssInfo[0]
+        ssPort = int(ssInfo[1])
+        createConnection(ssAddress, ssPort)
+    except OSError:
+        print("Could not open/read file " + fileName + "\nExiting...")
+        exit(1)
 
 
 def main():
@@ -17,11 +44,12 @@ def main():
     if numArgs == 1:
         # Read Local chain file
         URL = argv[0]
-        print("Reading chaingang.txt")
+        readFile(URL)
     elif numArgs == 3 and argv[1] == '-c':
         # Use custom chainfile
         URL = argv[0]
-        print("Reading custom chain file in argv[2]")
+        fileName = argv[2]
+        readFile(URL, fileName)
     else:
         printUsage()
 
