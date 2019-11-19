530920a5a1e53af3d164e7dea65577c534494e43
elavertu9 <elavertu@rams.colostate.edu>
2019-11-18 17:07:30 -0700

    removed reader.py because that file is named awget.py.... implemented basic arg parsing

--- awget.py
+++ awget.py
@@ -1,14 +1,29 @@
 import sys
+import getopt
 # Takes up to 2 command line args
     # awget <URL> [-c chainfile]
     # if no chainfile supplied, read the local file called chaingang.txt
     # else if problems with file, print error and exit
 
-argv = sys.argv[1:]
-argn = 0
+def printUsage():
+    print('Usage: awget.py <URL> -c <chainfile>')
+
 
 def main():
     print("Hello from awget.py")
+    argv = sys.argv[1:]
+    numArgs = len(argv)
+
+    if numArgs == 1:
+        # Read Local chain file
+        URL = argv[0]
+        print("Reading chaingang.txt")
+    elif numArgs == 3 and argv[1] == '-c':
+        # Use custom chainfile
+        URL = argv[0]
+        print("Reading custom chain file in argv[2]")
+    else:
+        printUsage()
 
 
 
